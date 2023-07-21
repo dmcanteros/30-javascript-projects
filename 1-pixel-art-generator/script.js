34446d1eb86de50de4a224bd4b00b7c276c2ade4
Daph Canteros <canterosdaphanemara@gmail.com>
2023-07-22 00:27:53 +0800

    added objects, create element, event listener
    • object events
    • create element
    • add event listener

--- 1-pixel-art-generator/script.js
+++ 1-pixel-art-generator/script.js
@@ -1,5 +1,6 @@
 /* THE PURPOSE OF THIS PROJECT IS TO GAIN AN IN-DEPTH UNDERSTANDING ABOUT THE BASIC CONCEPTS OF JAVASCRIPT
-THEREFORE, I AM GOING TO EXPLAIN THE LOGIC FLOW FOR FUTURE REFERENCE */
+THEREFORE, I AM GOING TO EXPLAIN THE LOGIC FLOW FOR FUTURE REFERENCE REVIEW */
+
 
 // DECLARING VARIABLES WITH CLASSNAME AND ID
 let container = document.querySelectorAll(".container");
@@ -13,8 +14,9 @@ let paintBtn = document.getElementById("#paint-btn");
 let widthValue = document.getElementById("#width-value");
 let heightValue = document.getElementById("#height-value");
 
-/*
-*/
+
+/* THE OBJECT EVENTS HAS 2 SETS OF INNER OBJECTS DECLARED AS MOUSE AND TOUCH.
+THE PROPERTIES AND VALUES REPRESENT THE EVENT INTERACTIONS */
 let events = {
     mouse: {
         down: "mousedown",
@@ -27,3 +29,70 @@ let events = {
         up: "touchend",
     },
 };
+
+
+/*
+*/
+let deviceType = "";
+
+let draw = false;
+let erase = false;
+
+const isTouchDevice = () => {
+    try {
+        document.createElement("TouchEvent");
+        deviceType = "touch";
+        return true;
+    } catch (e) {
+        deviceType = "mouse";
+        return false;
+    }
+};
+
+isTouchDevice();
+
+
+/*
+*/
+gridButton.addEventListener("click", () => {
+    container.innerHTML = "";
+    let count = 0;
+    for (let i = 0; i < gridHeight.value; i++) {
+        count += 2;
+        let div = document.createEvent("div");
+        div.classList.add("gridRow");
+
+        for (let j = 0; j < gridWidth.value; j++) {
+            count += 2;
+            let col = document.createEvent("div");
+            col.classList.add("gridCol");
+            col.setAttribute("id", `gridCol${count}`);
+            col.addEventListener(events[deviceType].down, () => {
+                draw = true;
+                if (erase) {
+                    col.style.backgroundColor = "transparent";
+                } else {
+                    col.style.backgroundColor = colorButton.value;
+                }
+            });
+
+            col.addEventListener(events[deviceType].move, (e) => {
+                let elementId = document.elementFromPoint (
+                    !isTouchDevice() ? e.clientX : e.touches[0].clientX,
+                    !isTouchDevice() ? e.clientY : e.touches[0].clientY,
+                ), id;
+                checker(elementId);
+            });
+
+            col.addEventListener(events[deviceType].up, () => {
+                draw = false;
+            });
+
+            div.appendChild(col);
+
+        }
+
+        container.appendChild(div);
+
+    }
+});
