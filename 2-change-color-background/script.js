85302db871ad931c1790e63d30c6179c900869c2
Daph Canteros <canterosdaphanemara@gmail.com>
2023-08-12 14:13:51 +0800

    new project added

---
+++ 2-change-color-background/script.js
@@ -0,0 +1,12 @@
+const index = 0;
+
+function changeColors() {
+    let colors = ["blue", "orange", "pink", "yellow", "purple", "red"];
+
+    document.getElementsByTagName("main")[0].
+    style.background = colors[index++];
+
+    if (index > colors.length - 1) {
+        index = 0;
+    }
+}
