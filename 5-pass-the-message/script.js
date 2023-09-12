9e5b21932f10b6aeb17ddf79bb0e0f84bd4c23c1
Daph Canteros <canterosdaphanemara@gmail.com>
2023-09-12 22:47:27 +0800

    project # 5 added: pass the message

---
+++ 5-pass-the-message/script.js
@@ -0,0 +1,11 @@
+const inputMessage = document.getElementById("input-message");
+
+inputMessage.addEventListener("keydown", function(event) {
+    if (event.key == "Enter")
+    promptMessage();
+});
+
+const promptMessage = () => {
+    document.getElementById("output-message").innerHTML = inputMessage.value;
+    inputMessage.value = "";
+}
