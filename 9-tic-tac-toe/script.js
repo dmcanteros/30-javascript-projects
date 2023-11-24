85b634dbb2d51e60d2c0714d07a00301b3e19ae9
Daph Canteros <canterosdaphanemara@gmail.com>
2023-11-24 17:29:35 +0800

    modal components created

--- 9-tic-tac-toe/script.js
+++ 9-tic-tac-toe/script.js
@@ -9,19 +9,70 @@ const gameBoard = [
 ];
 
 
+// "X" always play first
 let currentPlayer = "X";
 
 
-const boardDisplay = () => {
+/* const boardDisplay = () => {
     console.log("  0 1 2");
 
-    for (let i = 0; i < gameBoard.length; i++) {
-        let row = `${i} `;
+    // Navigating the gameBoard array that acts as full 2D board
+    for (let arr = 0; arr < gameBoard.length; arr++) {
+        let row = `${arr} `;
 
-        for (let j = 0; j < gameBoard[i].length; j++) {
-            row += gameBoard[i][j] + " ";
-            console.log(" " + gameBoard[i][j]);
+        // Iterates the length of the sub-arrays that acts as the rows of the board
+        for (let subArr = 0; subArr < gameBoard[arr].length; subArr++) {
+            row += gameBoard[arr][subArr] + " ";
+            console.log(" " + gameBoard[arr][subArr]);
         }
         console.log(row);
     }
+}; */
+
+
+const playerTurns = () => {
+
+    const tableBody = document.querySelector(".t3-board"); // Assigning the .t3-board section in HTML file
+
+    tableBody.innerHTML = ""; // Clear the previous game state (if any)
+
+
+    for (let arr = 0; arr < gameBoard.length; arr++) { // Initializing the length of the rows
+        const row = document.createElement("tr");
+
+        for (let subArr = 0; subArr < gameBoard[arr].length; subArr++) { // Getting the length of the gameBoard[arr] for sub-arrays to initialize the cell values
+            const cell = document.createElement("td");
+            cell.classList.add("cell");
+
+            cell.textContent = gameBoard[arr][subArr];
+
+            cell.addEventListener("click", () => { // Click event listener when cells are being clicked during player's movement
+                cellClick(arr, subArr); // Calls the function to iterate the event conditions <-- will be called on the next function
+            });
+
+            row.appendChild(cell);
+        }
+
+        tableBody.appendChild(row);
+    }
+};
+
+
+// Conditions how clicking the cells works as the player interacts
+const cellClick = (row, col) => {
+
+    if (gameBoard[row][col] === " ") { // Condition if the cell is empty
+        gameBoard[row][col] = currentPlayer; // Player's move will be executed based on the player's chosen symbol (X or O)
+
+        currentPlayer = currentPlayer === "X" ? "O" // Switch player
+        : "X";
+
+        boardDisplay(); // Current board with player's moves will be updated
+    }
 }
+
+
+// Executed immediately after the browser load is complete
+/* const execute = () => {
+    boardDisplay();
+} */
